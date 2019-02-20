9b99e02bb178932a00b2954b6d3433cbc5f1465c
liruqi <liruqi@gmail.com>
2019-02-20 10:38:13 +0800

    Add snippet

--- Snippet/MainWindow.xaml.cs
+++ Snippet/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,57 @@ namespace Snippet
         {
             InitializeComponent();
         }
+
+        private void onTextChangd(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine(TBInput.Text);
+            var key = RegexUtils.GetProfileOrHashtagKey(TBInput.Text, true);
+            if (key == null)
+            {
+                TBOutput.Text = "null";
+                return;
+            }
+            TBOutput.Text = key;
+        }
+    }
+
+    public class RegexUtils
+    {
+
+        private static readonly Regex TwitterProfilePattern =
+            new Regex(@"^http(s)?://(mobile\.)?(twitter)\.(com(\.\w{2})?)/(?<profileName>[\w]+)(/)?$",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private static readonly Regex TwitterHashtagPattern =
+            new Regex(@"^http(s)?://(mobile\.)?(twitter)\.(com(\.\w{2})?)/hashtag/(?<hashtag>[\w]+)(/)?$",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public static string GetProfileOrHashtagKey(string url, bool enableHashtag)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return null;
+            }
+
+            var match = TwitterProfilePattern.Match(url);
+            if (match.Success)
+            {
+                return "@" + match.Groups["profileName"].Value;
+            }
+
+            if (!enableHashtag)
+            {
+                return null;
+            }
+
+            match = TwitterHashtagPattern.Match(url);
+            if (!match.Success)
+            {
+                return null;
+            }
+
+            return "#" + match.Groups["hashtag"].Value;
+        }
+
     }
 }
